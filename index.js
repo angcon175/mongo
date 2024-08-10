09e79d853ffacd0432ae10dbec74d541ad7a894e
angcon175 <angcon26011998@gmail.com>
2024-08-10 21:23:29 +0600

    ..

---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+const user1 = new User({ name: "Angcon", email: "angcon@gmail.com", age: 24 });
+
+user1.save();
